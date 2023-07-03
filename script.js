68d0b5b08b43aacef1d6560630559ee389f80325
TheZaran <138419233+TheZaran@users.noreply.github.com>
2023-07-03 08:06:13 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+function to(){
+
+    var par = document.querySelector('.pr');
+
+    par.innerHTML = 'В КОРЗИНЕ';
+
+}
+function ex(){
+
+    var par = document.querySelector('.par');
+
+    par.innerHTML = 'В КОРЗИНЕ';
+
+}
+
+function sa(){
+
+    var par = document.querySelector('.p');
+
+    par.innerHTML = 'В КОРЗИНЕ';
+
+}
+function bs(){
+
+    var par = document.querySelector('.bs');
+
+    par.innerHTML = 'В КОРЗИНЕ';
+
+}
+function ln(){
+
+    var par = document.querySelector('.ln');
+
+    par.innerHTML = 'В КОРЗИНЕ';
+
+}
+function bn(){
+
+    var par = document.querySelector('.bn');
+
+    par.innerHTML = 'В КОРЗИНЕ';
+
+}
